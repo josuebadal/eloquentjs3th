1d30d79f6d27ddd45750b52c7aa2378427f2d5e6
JOSUE BADAL <josuebadal.jjbc@gmail.com>
2026-04-13 20:05:37 -0600

    Funciones de prueba sin modo estricto

---
+++ cap8/6ManejodeExcepciones.js
@@ -0,0 +1,23 @@
+//Ejercicio de la pagina 142 Manejo de Excepciones
+//se requiere del index para hacer las pruebas
+
+function pedirDireccion(pregunta){
+    let resultado = prompt(pregunta);
+    if (resultado.toLowerCase() == "izquierda") return "I";
+    if (resultado.toLowerCase() == "derecha") return "D";
+    throw new Error("Direccion Invalida: " + resultado);
+}
+
+function mirar(){
+    if (pedirDireccion("Hacia que direccion quieres ir ?") == "I"){
+        return "una casa";
+    } else {
+        return "dos osos furiosos";
+    }
+}
+
+try {
+    console.log("Tu vez", mirar());
+}   catch (error){
+    console.log("Algo incorrecto sucedio: " + error);
+};
