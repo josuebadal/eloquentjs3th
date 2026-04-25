53d989f763412d7626310934b45ef081228b2a4d
badal <josuebadal.jjbc@gmail.com>
2026-04-25 14:18:24 -0600

    Se agregan los ejercicios finales

--- cap9/5LimitesDeString.js
+++ cap9/5LimitesDeString.js
@@ -72,3 +72,72 @@ console.log(removerComentarios("1 /* a */+/* b */ 1"));
 // → 1 1
 console.log(removerComentarios("1 /* a */+/* b */ 1"));
 // → 1 + 1
+
+
+console.log("Ejercicios Finales del cap 5");
+//Ejercicios  finales del capitulo 5
+/*
+Code golf es un término utilizado para el juego de intentar expresar un programa en particular con la menor cantidad de caracteres posible. De manera
+similar, regexp golf es la práctica de escribir una expresión regular lo más pequeña posible para que coincida con un patrón dado, y solo ese patrón.
+Para cada uno de los siguientes elementos, escribe una expresión regular
+para comprobar si el patrón dado ocurre en una cadena. La expresión regular
+debe coincidir solo con cadenas que contengan el patrón. Cuando tu expresión
+funcione, verifica si puedes hacerla más pequeña.
+*/
+
+verify(/car[rt]/,
+    ["my car", "bad cats"],
+    ["camper", "high art"]);
+    
+verify(/pr?op/,
+    ["pop culture", "mad props"],
+    ["plop", "prrrop"]);
+
+verify(/ferr(et|y|ari)/, 
+    ["ferret", "ferry", "ferrari"],
+    ["ferrun", "transfer A"]);    
+
+verify(/ious($|\P{L})/u,
+    ["how delicious", "spacious room"],
+    ["ruinous", "consciousness"]);
+
+verify(/\s[.,:;]/,
+    ["bad punctuation ."],
+    ["escape the dot"]);
+
+verify(/\p{L}{7}/u,
+    ["Siebentausenddreihundertzweiundzwanzig"],
+    ["no", "three small words "]);
+
+verify(/(^|\P{L})[^\P{L}e]+($|\P{L})/ui,
+    ["read playypus", "wobbling nest"],
+    ["earth bed", "bedrøvet abe", "BEET"]); 
+
+function verify(regexp, yes, no){
+    if (regexp.source == "...") return;
+    for (let str of yes) if (!regexp.test(str)){
+        console.log(`Failure to match '${str}'`);
+    }
+    for (let str of no) if (regexp.test(str)){
+        console.log(`Unexpected match for '{str}'`);
+    }
+}
+
+console.log("EJERCICIO NUMERO 2");
+let text = "'I'm the cook , 'he said, 'it's my job.'";
+console.log(text.replace(/(^|\P{L})'|'(\P{L}|$)/gu, '$1"$2'));
+
+console.log("EJERCICIO NUMERO 3");
+let number = /^[+\-]?(\d+(\.\d*)?|\.\d+)([eE][+\-]?\d+)?$/;
+for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+    "1.3e2", "1E-4", "1e+12"]){
+        if (!number.test(str)){
+            console.log(`Failed to match '${str}'`);
+        }
+    }
+for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+    ".5.", "1f5", "."]){
+        if (number.test(str)){
+            console.log(`Incorrectly accepted '{str}'`);
+        }
+    }
