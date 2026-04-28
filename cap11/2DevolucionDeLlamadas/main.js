537d5ade269e8af1796cb1423fb06e2c124e96c7
JOSUE BADAL <josuebadal.jjbc@gmail.com>
2026-04-28 10:03:05 -0600

    ficheros nuevos del cap 11

---
+++ cap11/2DevolucionDeLlamadas/main.js
@@ -0,0 +1,4 @@
+//CAP 11 DEVOLUCION DE LLAMADAS
+// PAG 192 PDF 28-ABRIL-2026
+
+setTimeout (() => console.log("Tick"),1000);
--- cap11/main.js
+++
@@ -1,9 +0,0 @@
-import { granRoble } from "./tecnologia-cuervo.js";
-
-granRoble.leerAlmacenamiento("caches de alimentos", caches => {
-  let primerCache = caches[2];
-
-  granRoble.leerAlmacenamiento(primerCache, informacion => {
-    console.log(informacion);
-  });
-});
