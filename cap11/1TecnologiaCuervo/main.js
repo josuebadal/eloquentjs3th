dddf873acc7498e9c351aec21b07a7b860d13670
JOSUE BADAL <josuebadal.jjbc@gmail.com>
2026-04-28 09:58:39 -0600

    avances del capitulo 11

---
+++ cap11/1TecnologiaCuervo/main.js
@@ -0,0 +1,8 @@
+import { granRoble } from "./tecnologia-cuervo.js";
+
+granRoble.send(
+  "Pastura de Vacas",
+  "nota",
+  "Vamos a graznar fuerte a las 7PM",
+  () => console.log("Nota entregada.")
+);
---
+++ cap11/1TecnologiaCuervo/tecnologia-cuervo.js
@@ -0,0 +1,21 @@
+export const granRoble = {
+  almacen: {
+    "Pastura de Vacas": []
+  },
+  leerAlmacenamiento(nombre,callback){
+    setTimeout(() => {
+      callback(this.almacen[nombre]);
+    },100)
+  },
+  send(destino,tipo,mensaje,callback){
+    setTimeout(() =>{
+      if (!this.almacen[destino]) {
+        this.almacen[destino] = [];
+      }
+      this.almacen[destino].push({
+        tipo,mensaje
+      });
+      callback();
+    },100);
+  }
+};
